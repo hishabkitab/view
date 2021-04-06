1fdaf428399b1c57cff153c4b0c5265e5de35907
Hafijul Islam <hafijul233@gmail.com>
2021-04-06 13:00:31 +0600

    Logo added

--- resources/views/components/navbar/logo.blade.php
+++ resources/views/components/navbar/logo.blade.php
@@ -1,3 +1,4 @@
 @section('logo')
-<a href="../../index.html" class="df-logo">Hishab<span>Kitab</span></a>
+{{--<a href="../../index.html" class="df-logo">Hishab<span>Kitab</span></a>--}}
+    <a href="#" class="df-logo"><img src="{{asset('images/HK_logo.png')}}" height="36"></a>
 @endsection
